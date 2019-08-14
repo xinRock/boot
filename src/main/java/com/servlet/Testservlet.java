88a20f0b637e2cd61cf55cd5abddb82e74c6d4f4
xinRock <huanglaiye@163.com>
2019-08-14 15:38:54 +0800

    servlet

---
+++ src/main/java/com/servlet/Testservlet.java
@@ -0,0 +1,28 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Testservlet extends HttpServlet  {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("daowozhele");
+		System.out.println("daowozhele");
+		System.out.println("daowozhele");
+		resp.getWriter().write("Hello MyServlet");
+	}
+	
+}
